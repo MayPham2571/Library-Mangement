4209edfbbdbdab9c3f7f03223249226d7eab4b63
tht216 <73464495+tht216@users.noreply.github.com>
2021-01-07 14:17:42 +0700

    Final

---
+++ LibraryManagement(final)/src/IForgot.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ACER
+ */
+interface IForgot {
+   public abstract void SearchButton(java.awt.event.ActionEvent evt);
+   public abstract void RetrieveButton(java.awt.event.ActionEvent evt);
+   public abstract void BackButton(java.awt.event.ActionEvent evt);
+   public abstract void Retrieve();
+   public abstract void Search();
+   public abstract void UI();
+}
---
+++ LibraryManagement(final)/src/ILogin.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ACER
+ */
+interface ILogin {
+    public abstract void ForgotButton(java.awt.event.ActionEvent evt);
+    public abstract void LoginButton(java.awt.event.ActionEvent evt);
+    public abstract void SignupButton(java.awt.event.ActionEvent evt);
+    public abstract void UI();
+}
