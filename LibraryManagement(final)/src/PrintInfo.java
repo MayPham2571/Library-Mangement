c22187fa6af79e4cdcbb818cf0285b84edad8299
May <macbook@Mays-MBP.lan>
2021-01-08 09:39:53 +0700

    Adding diagram and report

--- LibraryManagement(final)/src/PrintInfo.java
+++ LibraryManagement(final)/src/PrintInfo.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -17,13 +20,10 @@ private static final PrintInfo print = new PrintInfo();
     }
 
 public void printWelcome(String name){
-	System.out.println("Welcome back, " + name + "!");
+	JOptionPane.showMessageDialog(null,"Welcome back, " + name + "!");
 }
 
-public void printDevInfo(){
-	System.out.println("Name: Phạm Nguyễn Ngọc Mai...");
-	
-}
+
 
 }
 
